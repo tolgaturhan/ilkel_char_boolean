30cdc6731a89c970f0fdf82c0e0c33d57caf85fd
Tolga Turhan <tolgaturhan@live.com>
2022-10-20 18:17:27 +0300

    v0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        char vChar = 'b';
+        char vChar2 = 98;
+
+        System.out.println(vChar);
+        System.out.println(vChar2);
     }
 }
